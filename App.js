fc41569a1cae6317582d6d03ed21dec39a51f35c
nithinarya2002 <nithinarya2002@gmail.com>
2024-09-26 20:34:48 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,86 @@
+/*
+// const heading = React.createElement(
+//     'h1',
+//     {id:"header",xyz:"abc"},
+//     'Hello world using ReactJS!'
+// );
+
+// Remember ReactElement(Object) when rendered gets converts into HTML(Browser Understands)
+
+// console.log(heading);
+
+
+
+<div id = "parent">
+    <div id="child1">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div>
+
+*/
+
+// use array of ReactElements to make siblings => HtML elements at one level
+
+// const parent = React.createElement(
+//     'div',
+//     {id:"parent"},
+//     React.createElement(
+//         'div',
+//         {id:"child"},
+//         [React.createElement(
+//             'h1',
+//             {},
+//             "I'm h1 tag"
+//         ),
+//         React.createElement(
+//             'h2',
+//             {},
+//             "I'm h2 tag"
+//         )
+//         ]
+//     )
+// );
+
+const parent = React.createElement(
+    'div',
+    {id:"parent"},
+    [React.createElement(
+        'div',
+        {id:"child1"},
+        [React.createElement(
+            'h1',
+            {},
+            "I'm h1 tag"
+        ),
+        React.createElement(
+            'h2',
+            {},
+            "I'm h2 tag"
+        )
+        ]
+    ),React.createElement(
+        'div',
+        {id:"child2"},
+        [React.createElement(
+            'h1',
+            {},
+            "I'm h1 tag"
+        ),
+        React.createElement(
+            'h2',
+            {},
+            "I'm h2 tag"
+        )
+        ]
+    )]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
