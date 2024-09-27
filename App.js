b29492078b2a426f443838ef440c9d32a33eac2b
nithinarya2002 <nithinarya2002@gmail.com>
2024-09-27 17:32:18 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /*
 // const heading = React.createElement(
 //     'h1',
@@ -46,41 +49,18 @@
 //     )
 // );
 
-const parent = React.createElement(
-    'div',
-    {id:"parent"},
-    [React.createElement(
-        'div',
-        {id:"child1"},
-        [React.createElement(
-            'h1',
-            {},
-            "I'm h1 tag"
-        ),
-        React.createElement(
-            'h2',
-            {},
-            "I'm h2 tag"
-        )
-        ]
-    ),React.createElement(
-        'div',
-        {id:"child2"},
-        [React.createElement(
-            'h1',
-            {},
-            "I'm h1 tag"
-        ),
-        React.createElement(
-            'h2',
-            {},
-            "I'm h2 tag"
-        )
-        ]
-    )]
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Starting with React
+
+# Parcel
+- Dev build
+- Local Server
+- Automatically refreshing the page
+- HMR => Hot Module Replacement
+- Uses a File Watching Algorithm - written in C++
+- Caching Mechanism is used which helps in Faster next builds.
