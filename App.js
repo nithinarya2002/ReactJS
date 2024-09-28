7965de73f1446e0158c3b01dcd1ad4d27af1de80
nithinarya2002 <nithinarya2002@gmail.com>
2024-09-28 13:15:27 +0530

    episode-03

--- App.js
+++ App.js
@@ -12,8 +12,9 @@ import ReactDOM from "react-dom/client";
 
 // console.log(heading);
 
+*/
 
-
+/*
 <div id = "parent">
     <div id="child1">
         <h1>I'm h1 tag</h1>
@@ -27,27 +28,28 @@ import ReactDOM from "react-dom/client";
 
 */
 
-// use array of ReactElements to make siblings => HtML elements at one level
+/*
+use array of ReactElements to make siblings => HtML elements at one level
 
-// const parent = React.createElement(
-//     'div',
-//     {id:"parent"},
-//     React.createElement(
-//         'div',
-//         {id:"child"},
-//         [React.createElement(
-//             'h1',
-//             {},
-//             "I'm h1 tag"
-//         ),
-//         React.createElement(
-//             'h2',
-//             {},
-//             "I'm h2 tag"
-//         )
-//         ]
-//     )
-// );
+const parent = React.createElement(
+    'div',
+    {id:"parent"},
+    React.createElement(
+        'div',
+        {id:"child"},
+        [React.createElement(
+            'h1',
+            {},
+            "I'm h1 tag"
+        ),
+        React.createElement(
+            'h2',
+            {},
+            "I'm h2 tag"
+        )
+        ]
+    )
+);
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
@@ -61,6 +63,47 @@ const parent = React.createElement("div", { id: "parent" }, [
 ]);
 
 console.log(parent);
+*/
+
+/*
+const heading = React.createElement("h1",{id:"header"},'Hello ReactJs');
+console.log(heading);
+*/
+
+/*
+JSX - HTML like or XML like syntax
+JSX => Babel transpiles it to React.createElement => ReactElement(Object) => HTML element(render)
+*/
+
+//React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Hello ReactJs Element using jsx
+  </h1>
+);
+console.log(heading); //prints an object
+
+/*
+const HeadingComponent = () => {
+  return <h1 className="heading">Hello ReactJs functional Component</h1>;
+};
+*/
+const Title = ()=>(
+    <h1 className="head" tabIndex="5">
+      Hello ReactJs using jsx
+    </h1>
+  );
+
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {heading /*Javascript inside JSX using curly braces*/} 
+    {Title()} 
+    <Title />
+    <Title></Title>
+    <h1 className="heading">Hello ReactJS functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
